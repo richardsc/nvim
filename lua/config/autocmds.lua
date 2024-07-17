6425ee826b9b31a3d254a4da2c3d16cb319c8b65
Clark Richards <clark.richards@gmail.com>
2024-07-17 20:53:06 -0300

    set option to disable global format-on-save

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- https://www.lazyvim.org/configuration/tips#disable-autoformat-for-some-buffers
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "r" },
+  callback = function()
+    vim.b.autoformat = false
+  end,
+})

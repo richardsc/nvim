f05e1f4f36963e5643bd9e069b3acb1ba7f7da28
Clark Richards <clark.richards@gmail.com>
2024-07-26 20:36:58 -0300

    fix deprecation warning for conform.nvim

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -3,8 +3,16 @@ return {
     -- see https://github.com/LazyVim/LazyVim/issues/51
     {
         "stevearc/conform.nvim",
-        opts = {
-            format = { timeout_ms = 60000 },
-        },
+        opts = function()
+            local opts = {
+                default_format_opts = {
+                    timeout_ms = 60000,
+                    async = false, -- not recommended to change
+                    quiet = false, -- not recommended to change
+                    lsp_format = "fallback", -- not recommended to change
+                },
+            }
+            return opts
+        end,
     },
 }

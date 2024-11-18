9e9d4278f4fcb310bd31b1f9c3e362bb90653177
Clark Richards <clark.richards@gmail.com>
2024-11-17 21:36:33 -0400

    update rlang after R.nvim update

--- lua/plugins/rlang.lua
+++ lua/plugins/rlang.lua
@@ -1,26 +1,27 @@
 return {
-  -- R support (see https://github.com/R-nvim/R.nvim)
-  {
-    "R-nvim/R.nvim",
-    lazy = false,
-    opts = { assignment_keymap = "_" },
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = { "markdown", "markdown_inline", "r", "rnoweb" },
-      })
-    end,
-  },
-  "R-nvim/cmp-r",
-  {
-    "hrsh7th/nvim-cmp",
-    -- filetypes = { "r", "rmd", "quarto", "rnoweb", "rhelp" },
-    config = function()
-      require("cmp").setup({ sources = { { name = "cmp_r" } } })
-      require("cmp_r").setup({})
-    end,
-  },
+    -- R support (see https://github.com/R-nvim/R.nvim)
+    {
+        "R-nvim/R.nvim",
+        lazy = false,
+        opts = { assignment_keymap = "_" },
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        run = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = { "markdown", "markdown_inline", "r", "rnoweb", "yaml" },
+                highlight = { enable = true },
+            })
+        end,
+    },
+    "R-nvim/cmp-r",
+    {
+        "hrsh7th/nvim-cmp",
+        -- filetypes = { "r", "rmd", "quarto", "rnoweb", "rhelp" },
+        config = function()
+            require("cmp").setup({ sources = { { name = "cmp_r" } } })
+            require("cmp_r").setup({})
+        end,
+    },
 }

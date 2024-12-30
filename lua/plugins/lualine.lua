4e34003734714b753c4e8d65e986d3eac7e82897
Clark Richards <clark.richards@gmail.com>
2024-12-29 22:23:37 -0400

    add undo-tree

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,9 +1,9 @@
 return {
-  {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    opts = function(_, opts)
-      table.insert(opts.sections.lualine_x, "filetype")
-    end,
-  },
+    {
+        "nvim-lualine/lualine.nvim",
+        event = "VeryLazy",
+        opts = function(_, opts)
+            table.insert(opts.sections.lualine_x, "filetype")
+        end,
+    },
 }

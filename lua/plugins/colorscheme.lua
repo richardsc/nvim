dba357ed694ddb4565174602bc3110c279c5f697
Clark Richards <clark.richards@gmail.com>
2024-12-30 17:48:57 -0400

    playing with colorscheme stuff

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,23 @@
 return {
+    -- {
+    --     "LazyVim/LazyVim",
+    --     opts = {
+    --         -- colorscheme = "catppuccin-frappe",
+    --         colorscheme = "tokyonight",
+    --     }
+    -- },
     {
-        "LazyVim/LazyVim",
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        style = "moon",
         opts = {
-            -- colorscheme = "catppuccin-frappe",
-            colorscheme = "tokyonight",
-        }
-    },
+            -- on_highlights = function(hl)
+            --     hl.comment = { bg = "#000000", fg = "#444444" }
+            --     hl.perlComment = { bg = "#000000", fg = "#444444" }
+            --     hl.Comment = { bg = "#000000", fg = "#444444" }
+            --     --hl.IlluminatedWordText = { bg = "#ffffff", fg = "#ffffff" }
+            -- end,
+        },
+    }
 }

a5b7433568c5daac8f34398edb2b7fb817d24eb8
Clark Richards <clark.richards@gmail.com>
2025-02-27 14:16:25 -0400

    Trying out the true-zen plugin for zen mode.
    Seems to have some issues in Lazyvim on Mint. Maybe just need to figure
    out how to use folke's zen mode instead

---
+++ lua/plugins/zen.lua
@@ -0,0 +1,48 @@
+return {
+    {
+        'pocco81/true-zen.nvim',
+        opts = {
+            --
+        },
+        config = function(_plugin, opts)
+            require('true-zen').setup(opts)
+        end,
+        keys = {
+            {
+                '<leader>zn',
+                '<cmd>TZNarrow<cr>',
+                mode = 'n',
+                desc = 'Narrow focused',
+                noremap = true,
+            },
+            {
+                '<leader>zn',
+                "<cmd>'<,'>TZNarrow<cr>",
+                mode = 'v',
+                desc = 'Ranged narrow focused',
+                noremap = true,
+            },
+            {
+                '<leader>zf',
+                '<cmd>TZFocus<cr>',
+                mode = 'n',
+                desc = 'Focued mode',
+                noremap = true,
+            },
+            {
+                '<leader>zm',
+                '<cmd>TZMinimalist<cr>',
+                mode = 'n',
+                desc = 'Minimalist mode',
+                noremap = true,
+            },
+            {
+                '<leader>za',
+                '<cmd>TZAtaraxis<cr>',
+                mode = 'n',
+                desc = 'Ataraxis mode',
+                noremap = true,
+            },
+        },
+    }
+}

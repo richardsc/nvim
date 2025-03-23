44da7ef43937d0f828150e93e17aa755249e3e36
Clark Richards <clark.richards@gmail.com>
2025-03-22 23:17:38 -0300

    undo what I did to fix the preview, because what it seems to do is disable vimtex ... :(

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -5,7 +5,7 @@ return {
         -- tag = "v2.15", -- uncomment to pin to a specific release
         init = function()
             -- vim.g.vimtex_complete_enabled = 0 -- Disable auto-completion for references
-            vim.g.vimtex_complete_bib = 0 -- Disable bibliography autocompletion
+            -- vim.g.vimtex_complete_bib = 0 -- Disable bibliography autocompletion
             -- vim.g.vimtex_echo_verbose_input = 0 -- Prevent verbose input echoes
             -- VimTeX configuration goes here, e.g.
             -- vim.g.vimtex_view_method = "zathura"

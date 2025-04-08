3c5beecdc7c7af40693599d4c46d0412f4408a7e
Clark Richards <clark.richards@gmail.com>
2025-04-08 12:24:36 -0300

    trying to figure out what is wrong with my treesitter/formatter in R ...
    Dan's config works (finds the r_language_server as a formatter), while
    mine doesn't. No idea why. Giving up for now and just configuring to use
    styler

--- lua/plugins/rlang.lua
+++ lua/plugins/rlang.lua
@@ -16,7 +16,7 @@ return {
                         elseif vim.bo.filetype == "rmd" or vim.bo.filetype == "quarto" then
                             vim.api.nvim_buf_set_keymap(0, "i", "`", "<Plug>RmdInsertChunk", { noremap = true })
                         end
-                    end
+                    end,
                 },
                 pdfviewer = "",
                 R_app = "radian",
@@ -25,13 +25,13 @@ return {
                 bracketed_paste = true,
                 min_editor_width = 72,
                 rconsole_width = 78,
-                objbr_mappings = {                                  -- Object browser keymap
-                    c = 'class',                                    -- Call R functions
-                    ['<localleader>gg'] = 'head({object}, n = 15)', -- Use {object} notation to write arbitrary R code.
+                objbr_mappings = { -- Object browser keymap
+                    c = "class", -- Call R functions
+                    ["<localleader>gg"] = "head({object}, n = 15)", -- Use {object} notation to write arbitrary R code.
                     v = function()
                         -- Run lua functions
-                        require('r.browser').toggle_view()
-                    end
+                        require("r.browser").toggle_view()
+                    end,
                 },
                 disable_cmds = {
                     "RClearConsole",
@@ -51,15 +51,29 @@ return {
         end,
     },
     {
-        "nvim-treesitter/nvim-treesitter",
-        run = ":TSUpdate",
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                ensure_installed = { "markdown", "markdown_inline", "r", "rnoweb", "yaml", "latex" },
-                highlight = { enable = true },
-            })
-        end,
+        "neovim/nvim-lspconfig",
+        opts = {
+            servers = {
+                r_language_server = {
+                    root_dir = function(fname)
+                        return require("lspconfig.util").root_pattern("DESCRIPTION", "NAMESPACE", ".Rbuildignore")(
+                            fname
+                        ) or require("lspconfig.util").find_git_ancestor(fname) or vim.loop.os_homedir()
+                    end,
+                },
+            },
+        },
     },
+    -- {
+    --     "nvim-treesitter/nvim-treesitter",
+    --     run = ":TSUpdate",
+    --     config = function()
+    --         require("nvim-treesitter.configs").setup({
+    --             ensure_installed = { "markdown", "markdown_inline", "r", "rnoweb", "yaml", "latex" },
+    --             highlight = { enable = true },
+    --         })
+    --     end,
+    -- },
     -- "R-nvim/cmp-r",
     -- {
     --     "hrsh7th/nvim-cmp",

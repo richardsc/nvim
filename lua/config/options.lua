bad896db45a6a87fcfdeab3af6d91c65f0f8aac4
Clark Richards <clark.richards@gmail.com>
2025-04-13 13:42:28 -0300

    add python extra and fix options "fuff" -> "ruff" typo

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,7 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.lazyvim_python_lsp = "pyright"
-vim.g.lazyvim_python_fuff = "ruff_lsp"
+-- vim.g.lazyvim_python_ruff = "ruff_lsp"
+vim.g.lazyvim_python_ruff = "ruff"
 vim.o.tabstop = 4
 vim.o.expandtab = true
 vim.o.softtabstop = 4

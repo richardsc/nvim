e1e71f778cf1cc18bdf3f8fe838fb2e068696fb6
Clark Richards <clark.richards@gmail.com>
2025-04-28 18:43:40 -0300

    trying some changes to better match's dan's config to see if I can get lsp to work in Rnw files

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,22 +1,6 @@
 return {
-    "neovim/nvim-lspconfig",
+    'neovim/nvim-lspconfig',
     opts = {
-        autoformat = false,
-        -- inlay_hints = { enabled = false },
+        document_highlight = { enabled = false },
     },
-    -- config = function()
-    --     vim.api.nvim_create_autocmd("LspAttach", {
-    --         callback = function(args)
-    --             local client = vim.lsp.get_client_by_id(args.data.client_id)
-    --             if client and client.name == "texlab" then
-    --                 -- Force disable inlay hints for texlab
-    --                 vim.schedule(function()
-    --                     if vim.lsp.inlay_hint then -- Ensure Neovim 0.10+ API exists
-    --                         vim.lsp.inlay_hint(args.buf, false)
-    --                     end
-    --                 end)
-    --             end
-    --         end,
-    --     })
-    -- end,
 }

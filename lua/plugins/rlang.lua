1d0cb87ed53d4f710cb2c47b11c4b08b2cbcec2d
Clark Richards <clark.richards@gmail.com>
2025-05-06 08:17:09 -0300

    cleanup

--- lua/plugins/rlang.lua
+++ lua/plugins/rlang.lua
@@ -65,23 +65,4 @@ return {
             },
         },
     },
-    -- {
-    --     "nvim-treesitter/nvim-treesitter",
-    --     run = ":TSUpdate",
-    --     config = function()
-    --         require("nvim-treesitter.configs").setup({
-    --             ensure_installed = { "markdown", "markdown_inline", "r", "rnoweb", "yaml", "latex" },
-    --             highlight = { enable = true },
-    --         })
-    --     end,
-    -- },
-    -- "R-nvim/cmp-r",
-    -- {
-    --     "hrsh7th/nvim-cmp",
-    --     -- filetypes = { "r", "rmd", "quarto", "rnoweb", "rhelp" },
-    --     config = function()
-    --         require("cmp").setup({ sources = { { name = "cmp_r" } } })
-    --         require("cmp_r").setup({})
-    --     end,
-    -- },
 }
